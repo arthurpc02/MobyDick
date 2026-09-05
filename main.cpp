84633d400ca12c484fde0803f9e3c2e6fa82e613
Arthur P. Cortes <arthurpc02@gmail.com>
2026-09-05 20:39:58 -0300

    add player input. use namespace std explicitly.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-using namespace std;
-
 namespace TermForm {
     constexpr const char* reset = "\033[0m";
     constexpr const char* black = "\033[30m";
@@ -22,26 +20,29 @@ class Character
 
         }
 
-        virtual void say(const char *text){}
+        virtual void say(const std::string& text){}
 };
 
 class Narrator : public Character
 {
     public:
-    void say(const char *text)
+    void say(const std::string& text)
     {
-        cout << "** " << text << endl;
+        std::cout << text << std::endl;
     }
 };
 
 class Stranger : public Character
 {
     public:
-    void say(const char *text)
+    void say(const std::string& text)
     {
-        cout << TermForm::cyan << "Stranger: " << text << TermForm::reset << endl;
+        std::cout << TermForm::cyan << "[Stranger] " << text << TermForm::reset << std::endl;
     }
 };
+
+std::string player_lastName;
+std::string player_firstName;
 
 int main()
 {
@@ -51,13 +52,24 @@ int main()
     narrator.say("You wake up. It's still raining. Very lightly.");
     narrator.say("The smell of seawater is already present in the air");
     narrator.say("The horses seem to go even slower than before. Until they stop");
+    std::cin.get();
     narrator.say("...");
     narrator.say("Your coachman is talking to a stranger outside, on the front porch of what seems to be an inn.");
     narrator.say("You jump out of the carriage and join them.");
 
     stranger.say("Good Evening.");
-    stranger.say("I need your name.");
+    stranger.say("I need your last name.");
 
+    std::cin >> player_lastName;
+    
+    stranger.say(player_lastName + ", right?");
+    stranger.say("Now your first name:");
+    
+    std::cin >> player_firstName;
+    
+    stranger.say(player_firstName + " " + player_lastName + "?");
+    narrator.say("He laughs loud. One time only. And then stops. The coachman is bored and wants to be paid.");
+    std::cin.get();
     narrator.say("You notice your coachman has surrounded and gripped you from behind.");
     narrator.say("You are stabbed by the stranger.");
 
