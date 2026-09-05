7e3a806accf859e4cc9a48ec6d71136ce5277cab
Arthur P. Cortes <arthurpc02@gmail.com>
2026-09-05 11:38:39 -0300

    color formatting

--- main.cpp
+++ main.cpp
@@ -1,11 +1,34 @@
-#include <stdlib.h>
 #include <iostream>
 
 using namespace std;
 
+namespace TermForm {
+    constexpr const char* reset = "\033[0m";
+    constexpr const char* black = "\033[30m";
+    constexpr const char* red = "\033[31m";
+    constexpr const char* green = "\033[32m";
+    constexpr const char* yellow = "\033[33m";
+    constexpr const char* blue = "\033[34m";
+    constexpr const char* purple = "\033[35m";
+    constexpr const char* cyan = "\033[36m";
+    constexpr const char* white = "\033[37m";
+}
+
+
+
 int main()
 {
-    cout<<"Hello World!\n";
+    std::cout << TermForm::red << "Red text" << TermForm::reset << "\n";
+//           ^red code^                ^reset^
+    cout << "** Está chovendo lá fora."<< endl;
+    cout<<"** You can hear thunder strucking in the distant."<<endl;
+    cout<<"** The horses seem to go even slower than before."<<endl;
+    cout<<"** The horses seem to go even slower than before."<<endl;
 
-    cout<<"\nGood-bye Cruel World!\n";
+    cout<< TermForm::cyan << "Stranger: Good Evening."<< TermForm::reset <<endl;
+    cout<<"Stranger: We were expecting Mr. Cortes. Who are you?"<<endl;
+
+
+
+    cout<<"\n** Good-bye Cruel World!"<<endl;
 }
